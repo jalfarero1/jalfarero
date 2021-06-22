9aabb0cd0ba221e50f4731dd14b8f4a9d2e036d8
jalfarero1 <84394780+jalfarero1@users.noreply.github.com>
2021-06-21 22:54:54 -0400

    Update a1_jalfarero.py

--- a1_jalfarero.py
+++ a1_jalfarero.py
@@ -5,9 +5,9 @@
     Please personlize the following author declaration:
 -----------------------------------------------------------------------
 OPS435 Assignment 1 - Winter 2021
-Program: a1_jalfarero.py
+Program: a1_jalfarero.py (replace [Seneca_name] with your Seneca User name)
 Author: Jordan Alfarero
-The python code in this file a1_jalfarero.py is original work written by
+The python code in this file (a1_jalfarero.py) is original work written by
 Jordan Alfarero. No code in this file is copied from any other source 
 except those provided by the course instructor, including any person, 
 textbook, or on-line resource. I have not shared this python script 
@@ -17,91 +17,62 @@ violators will be reported and appropriate action will be taken.
 '''
 import os
 import sys
-import calendar
 
-user_raw_data = sys.argv[1]
-
-#user_raw_data1 = user_raw_data.replace("/", "")
-#user_raw_data2 = user_raw_data1.replace(".", "")
-#sanitize = user_raw_data2.replace("-", "")
-#allow_chars = '0123456789'
-
-def size_check(sanitize,):
-    if len(sanitize) != 8:
-         x = False
-    else:
-         x = True
-
-    return x
-
-def sanitize(user_raw_data):
-    sanitize_data = ''
-    for i in user_raw_data:
-        if i == '/':
-             outchar = ''
-        elif i == '-':
-             outchar = ''
-        elif i == '.':
-             outchar = ''
-        sanitize_data += outchar
-        return sanitize_data
-
-def leap_year(l_year):
+def leap_year(obj):
     """
-    Takes the year from sanitized user input (YYYY) and returns it True if it is a leap year
-    or false if it is not a leap year.
+    Takes the year from user input YYYY and return it True if it is a leap year
+    or False if it is not a leap year.
     """
-    l_year = int(sanitize[0:4])
-    if calendar.isleap(l_year):
-       x = True 
+    if (obj % 4) ==0:
+       if (obj % 100) ==0:
+          if (obj % 400) ==0:
+             output = True
+          else:
+             output = False
+       else:
+          output = True
     else:
-       x = False  
-    
-    return x
-def range_check(l_year,years):
+      output = False
+    return output
+
+def sanitize(obj1,obj2):
+    """
+    Takes out characters that would otherwise disrupt the functionality of the script
+    and ensures only required values remain
+    """
+    cleanse = ''
+    for char in obj1:
+        if char in obj2:
+           cleanse += char
+    return cleanse
+
+def size_check(obj, intobj):
+    """
+    Determines the appropriate size for user input
+    """
+    if len(obj) == intobj:
+         output = True
+    else:
+         output = False
+    return output
+
+def range_check(obj1,obj2):
     """
     Takes the year from sanitized user input (YYYY) and determines if it falls within
     the appropriate range of 1900 and 9999.
     """
-    years = (1900, 9999)
-    if int(l_year) in years[0:1]:
-       x = True
+    if int(obj1) in range(obj2[0],obj2[1]+1):
+       output = True
     else:
-       x = False
-
-    return x
-
-#    if y_year <= yearmin:
-#         return False
-#    elif y_year >= yearmax:
-#         return False
-#    else:
-#         return True
-
-def month_check(x_month):
-    x_month = sanitize[4:6]
-    if 1 <= x_month <= 12:
-         x = True
-    else:
-         x = False
- 
-    return x
-
-def day_check(x_day, int):
-    x_day = int(sanitize[6:])
-    if x_day > 31:
-         x = False
-    else:
-         x = True
+       output = False  
    
-    return x
+    return output
 
 def usage():
-    
-    use = """
-    Usage: a1_jalfarero.py YYYYMMDD|YYYY/MM/DD|YYYY-MM-DD|YYYY.MM.DD
-
     """
+    Message on script's use if it is not being used for it's purpose
+    """ 
+    use = """Usage: a1_jalfarero.py YYYYMMDD|YYYY/MM/DD|YYYY-MM-DD|YYYY.MM.DD"""
     print(use)
     exit()
 
@@ -111,44 +82,37 @@ if __name__ == "__main__":
       sys.exit()
    month_name = ['Jan','Feb','Mar','Apr','May','Jun',
                  'Jul','Aug','Sep','Oct','Nov','Dec']
-   days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30}
+   days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
    user_raw_data = sys.argv[1]
+   # step 3
    allow_chars = '0123456789'
-   dob = sanitize
-   result = size_check
-#   if len(sanitize) == 8 and sanitize.isdigit():
-#       print("Sanitized user data:", dob)
-#   else:
-#       print("Error 09: wrong date entered")
-#       sys.exit()
+   dob = sanitize(user_raw_data, allow_chars)
+   # ste 4
+   result = size_check(dob,8)
+   if result == False:
+       print("Error 09: wrong date entered")
+       sys.exit()
+   # step 5
    year = int(dob[0:4])
    month = int(dob[4:6])
    day = int(dob[6:])
-#   result = range_check
-#   if result == False:
-   if year < 1900: 
+   # step 6 
+   result = range_check(year,(1900,9999))
+   if result == False: 
        print("Error 10: year out of range, must be 1900 or later")
        sys.exit()
-   if month > 12:
-       print("Error 02: Wrong month entered")
+   result = range_check(month,(1,12))
+   if result == False:
+       print("Error 02: wrong month entered")
        sys.exit()
-   elif month < 1:
-       print("Error 02: Wrong month entered")
-       sys.exit()
-#   result = leap_year
-   if calendar.isleap(year):
+   result = leap_year(year)
+   if result == True:
        days_in_month[2] = 29
-   else:
-       print("Error 03: wrong day entered")
-#   result = day_check
-   if day > 31:
+   result = range_check(day, (1, days_in_month[month]))
+   if result == False:
        print("Error 03: wrong day entered")
        sys.exit()
-   elif day < 1:
-       print("Error 03: wrong day entered")
-       sys.exit()
-#   elif day != days_in_month
-#       print("Error 03: wrong day entered")
-#       sys.exit()
+   # step 7
    new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
-   print("Your date of birth is:", new_dob)
+#   print("Your date of birth is:", new_dob)
+   print(new_dob)
